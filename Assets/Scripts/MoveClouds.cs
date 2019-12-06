d70ac1b5a95e59a7bdef6b25da7c6445c782fc69
Arutomu <43913712+Arutomoss@users.noreply.github.com>
2019-12-06 16:10:37 +0300

    Add flying clouds in main menu

---
+++ Assets/Scripts/MoveClouds.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveClouds : MonoBehaviour
+{
+    private float speed = 0.1f;
+
+    private void FixedUpdate() {
+        transform.Translate(speed * Time.deltaTime, 0, 0);  
+    }
+
+    // void FixedUpdate()
+    // {
+    //     transform.Translate(speed * Time.deltaTime, 0, 0);
+
+    //     if (transform.position.x > 13f)
+    //     {
+    //         Destroy(this.gameObject);
+    //     }   
+    // }
+}
---
+++ Assets/Scripts/SpawnClouds.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnClouds : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cloud;
+    private Vector3 cloud_tr;
+
+    private void Start()
+    {
+        cloud_tr = cloud.transform.localScale;
+        StartCoroutine(Spawn());
+    }
+
+    // void Update()
+    // {
+    //     if (transform.position.x > 13f)
+    //     {
+    //         Destroy(cloud);
+    //     }
+    // }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(14f, 25f));
+            cloud.transform.localScale = cloud_tr;
+            cloud.transform.localScale -= new Vector3(Random.Range(0.05f, 0.2f), Random.Range(0.05f, 0.2f), 0);
+            Instantiate(cloud, new Vector2(-12f, Random.Range(2.5f, 5f)), Quaternion.identity);
+        }
+    }
+}
